bfe89ff77adecb573e98953a1ff60505827d7abd
Aaron Zorgel <azorgel@MacBook-Pro-9.local>
2026-03-25 14:02:41 -0400

    Always use web client for full format access; cookies used when available

--- main.py
+++ main.py
@@ -52,10 +52,10 @@ def get_extra_args():
     args = []
     if COOKIES_FILE.exists():
         args += ["--cookies", str(COOKIES_FILE)]
-        args += ["--extractor-args", "youtube:player_client=web"]
-        args += ["--remote-components", "ejs:github"]
-    else:
-        args += ["--extractor-args", "youtube:player_client=android,ios"]
+    args += [
+        "--extractor-args", "youtube:player_client=web",
+        "--remote-components", "ejs:github",
+    ]
     return args
 
 # Quality presets shown to the user.
